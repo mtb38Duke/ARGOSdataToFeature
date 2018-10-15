db7e7297992b3e724efaad98bd8ba45b1ed06d9c
mtb38Duke <42784304+mtb38Duke@users.noreply.github.com>
2018-10-15 12:36:15 -0400

    Gets tagID from lineList

--- ImportARGOS.py
+++ ImportARGOS.py
@@ -16,3 +16,23 @@ import sys, os, arcpy
 # Set input variables (Hard-wired)
 inputFile = 'V:/ARGOSTracking/data/ARGOSData/1997dg.txt'
 outputFC = "V:/ARGOSTracking/scratch/ARGOStrack.shp"
+
+# open the ARGOS data file for reading
+inputFileObj = open(inputFile, 'r')
+
+# Get the first line of data so we can loop
+lineString = inputFileObj.readline()
+while lineString:
+    #run code only if line contains Date :
+    if("Date :" in lineString):
+
+    #split line into a list
+        lineList = lineString.split()
+
+        tagID = lineData[0]
+        break
+    
+    #get the next line
+    lineString = inputFileObj.readline()
+
+    
